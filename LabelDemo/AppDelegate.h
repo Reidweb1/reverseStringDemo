93dbe1c699144fd9c9b53749003993c4f0bbdbae
Reid Weber <reid_weber@hotmail.com>
2015-02-02 13:46:59 -0800

    Initial Commit

---
+++ LabelDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LabelDemo
+//
+//  Created by Reid Weber on 2/2/15.
+//  Copyright (c) 2015 Reid Weber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LabelDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LabelDemo
+//
+//  Created by Reid Weber on 2/2/15.
+//  Copyright (c) 2015 Reid Weber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
